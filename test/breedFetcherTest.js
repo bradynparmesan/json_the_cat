3f6b145626b16d1db07a49966586e60de241be31
Bradyn <bradyn.parisian@gmail.com>
2021-02-12 04:15:31 +0000

    test added, with linting completed

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -4,16 +4,19 @@ const { fetchBreedDescription } = require('../breedFetcher');
 const { assert } = require('chai');
 
 describe('fetchBreedDescription', () => {
-  it('returns a string description for a valid breed, via callback', (done) => {
-    fetchBreedDescription('Siberian', (err, desc) => {
-      // we expect no error for this scenario
-      assert.equal(err, null);
+  it('returns a correct string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (error, breedDescription) => {
+      assert.equal(error, null);
+      const expDescription = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+      assert.equal(expDescription, breedDescription.trim()); // test to compare fetched description to expected
+      done();
+    });
+  });
 
-      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
-
-      // compare returned description
-      assert.equal(expectedDesc, desc.trim());
-
+  it('returns an undefined description for an invalid/non-existent breed, via callback', (done) => {
+    fetchBreedDescription('cheshire', (error, breedDescription) => {
+      assert.equal(error, null);
+      assert.equal(breedDescription, undefined); // tests for non-existent breed and incorrect breed entry
       done();
     });
   });
